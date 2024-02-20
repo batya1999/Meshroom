05f2c2a79e19e90d665f004bf970efe329067a95
Bat-ya <batya.ashkenazi@gmail.com>
2024-02-20 16:22:34 +0000

    Override start.sh to fit meshroom:latest
    Ignore venv
    Ignore pics directory

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 export MESHROOM_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )"
-export PYTHONPATH=$MESHROOM_ROOT:$PYTHONPATH
-
+export PYTHONPATH=${MESHROOM_ROOT}:${PYTHONPATH}
 # using existing alicevision release
-#export LD_LIBRARY_PATH=/foo/Meshroom-2023.2.0/aliceVision/lib/
-#export PATH=$PATH:/foo/Meshroom-2023.2.0/aliceVision/bin/
-
-# using alicevision built source
-#export PATH=$PATH:/foo/build/Linux-x86_64/
-
-python "$MESHROOM_ROOT/meshroom/ui"
+export LD_LIBRARY_PATH=${AV_BUNDLE}/lib/
+export PATH=$PATH:${AV_BUNDLE}/bin/
+python ${MESHROOM_ROOT}/meshroom/ui
